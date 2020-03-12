8725bee136f7e541013c44cdfa1958088fb14bd5
Bryan Morrow <bkmorrow@gmail.com>
2020-03-12 10:24:46 -0500

    add image layer history
    v2/images/registry/repo/tag/history_layers

---
+++ client/image_layers.go
@@ -0,0 +1,73 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/BryanKMorrow/aqua-sdk-go/types/images"
+	"github.com/parnurzeal/gorequest"
+)
+
+// GetLayers - retrieves all layers from a partcular image
+// Accepts the registry, repo and tag strings as well as the page number, pagesize and params map
+// Returns response struct, remaining count and next page
+// v2/images
+func (cli *Client) GetLayers(registry, repo, tag string, page, pagesize int, params map[string]string) (images.Layers, int, int, int) {
+	// set the default pagesize
+	if pagesize == 0 {
+		pagesize = 1000
+	}
+	var response = images.Layers{}
+	request := gorequest.New()
+	request.Set("Authorization", "Bearer "+cli.token)
+	apiPath := fmt.Sprintf("/api/v2/images/%s/%s/%s/history_layers", registry, repo, tag)
+	paramString := cli.GetParams(params)
+	events, body, errs := request.Clone().Get(cli.url+apiPath).Param("page", strconv.Itoa(page)).Param("pagesize", strconv.Itoa(pagesize)).
+		Query(paramString).End()
+	log.Printf("Calling %s%s", cli.url, apiPath)
+	if errs != nil {
+		log.Println(events.StatusCode)
+	}
+	if events.StatusCode == 200 {
+		err := json.Unmarshal([]byte(body), &response)
+		if err != nil {
+			log.Printf("Error calling func GetLayers from %s%s, %v ", cli.url, apiPath, err.Error())
+			//json: Unmarshal(non-pointer main.Request)
+		}
+	}
+	// remove duplicate layers
+	newLayers := removeDuplicates(response)
+	// create new Layer{} for response
+	var newResponse = images.Layers{
+		Count:    response.Count,
+		Page:     response.Page,
+		Pagesize: response.Pagesize,
+		Result:   newLayers,
+	}
+
+	remaining := cli.CalcRemaining(pagesize, page, response.Count)
+	page = response.Page + 1
+	remaining, next := cli.CalcNext(remaining, page)
+	return newResponse, remaining, next, response.Count
+}
+
+func removeDuplicates(response images.Layers) images.LayerResult {
+	// Use map to record duplicates as we find them.
+	encountered := map[string]bool{}
+	result := images.LayerResult{}
+
+	for _, layer := range response.Result {
+		if encountered[layer.ID] == true {
+			// Do not add duplicate.
+		} else {
+			// Record this element as an encountered element.
+			encountered[layer.ID] = true
+			// Append to result slice.
+			result = append(result, layer)
+		}
+	}
+	// Return the new slice.
+	return result
+}
--- types/images/layers.go
+++ types/images/layers.go
@@ -1,1 +1,42 @@
 package images
+
+// Layers response from v2/images/registry/repo/tag/history_layers
+type Layers struct {
+	Count    int `json:"count"`
+	Page     int `json:"page"`
+	Pagesize int `json:"pagesize"`
+	Result   []struct {
+		ID            string `json:"id"`
+		Comment       string `json:"comment"`
+		Created       int    `json:"created"`
+		CreatedBy     string `json:"created_by"`
+		Size          int    `json:"size"`
+		Position      int    `json:"position"`
+		CritVulns     int    `json:"crit_vulns"`
+		HighVulns     int    `json:"high_vulns"`
+		MedVulns      int    `json:"med_vulns"`
+		LowVulns      int    `json:"low_vulns"`
+		NegVulns      int    `json:"neg_vulns"`
+		SensitiveData int    `json:"sensitive_data"`
+		VulnsFound    int    `json:"vulns_found"`
+		IsBaseLayer   bool   `json:"is_base_layer"`
+	} `json:"result"`
+}
+
+// LayerResult - an attempt to de-duplicate layer history
+type LayerResult []struct {
+	ID            string `json:"id"`
+	Comment       string `json:"comment"`
+	Created       int    `json:"created"`
+	CreatedBy     string `json:"created_by"`
+	Size          int    `json:"size"`
+	Position      int    `json:"position"`
+	CritVulns     int    `json:"crit_vulns"`
+	HighVulns     int    `json:"high_vulns"`
+	MedVulns      int    `json:"med_vulns"`
+	LowVulns      int    `json:"low_vulns"`
+	NegVulns      int    `json:"neg_vulns"`
+	SensitiveData int    `json:"sensitive_data"`
+	VulnsFound    int    `json:"vulns_found"`
+	IsBaseLayer   bool   `json:"is_base_layer"`
+}

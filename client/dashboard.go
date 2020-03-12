16bcf78266f016da078e8c655753080bd79048bd
Bryan Morrow <bkmorrow@gmail.com>
2020-03-12 15:56:58 -0500

    dashboard overview get

---
+++ client/dashboard.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/BryanKMorrow/aqua-sdk-go/types/dashboard"
+	"github.com/parnurzeal/gorequest"
+)
+
+// GetOverview retrieves the first page dashboard statistics from v1/dashboard
+// query parameters registry=&hosts=&containers_app=
+// hosts doesn't do anything in this query, should call the hosts api directly
+// containers_app refers to Aqua Services
+// response is Overview struct
+func (cli *Client) GetOverview(paramsString map[string]string) dashboard.Overview {
+	var response = dashboard.Overview{}
+	request := gorequest.New()
+	request.Set("Authorization", "Bearer "+cli.token)
+	apiPath := fmt.Sprintf("/api/v1/dashboard")
+	paramString := cli.GetStringParams(paramsString)
+	events, body, errs := request.Clone().Get(cli.url + apiPath).Query(paramString).End()
+	log.Printf("Calling %s%s", cli.url, apiPath)
+	if errs != nil {
+		log.Println(events.StatusCode)
+	}
+	if events.StatusCode == 200 {
+		err := json.Unmarshal([]byte(body), &response)
+		if err != nil {
+			log.Printf("Error calling func GetOverview from %s%s, %v ", cli.url, apiPath, err.Error())
+			//json: Unmarshal(non-pointer main.Request)
+		}
+	}
+	return response
+}
---
+++ types/dashboard/dashboard.go
@@ -0,0 +1,76 @@
+package dashboard
+
+// Overview is the response from v1/dashboard
+type Overview struct {
+	Filter struct {
+		Application string `json:"application"`
+		Registry    string `json:"registry"`
+		Hosts       string `json:"hosts"`
+	} `json:"filter"`
+	RunningContainers struct {
+		Total        int `json:"total"`
+		Critical     int `json:"critical"`
+		High         int `json:"high"`
+		Medium       int `json:"medium"`
+		Low          int `json:"low"`
+		Ok           int `json:"ok"`
+		Unregistered int `json:"unregistered"`
+	} `json:"running_containers"`
+	RegistryCounts struct {
+		Images struct {
+			Total    int `json:"total"`
+			Critical int `json:"critical"`
+			High     int `json:"high"`
+			Medium   int `json:"medium"`
+			Low      int `json:"low"`
+			Ok       int `json:"ok"`
+		} `json:"images"`
+		ImagesTrends    interface{} `json:"images_trends"`
+		Vulnerabilities struct {
+			Total    int `json:"total"`
+			Critical int `json:"critical"`
+			High     int `json:"high"`
+			Medium   int `json:"medium"`
+			Low      int `json:"low"`
+			Ok       int `json:"ok"`
+		} `json:"vulnerabilities"`
+		CvesTrends interface{} `json:"cves_trends"`
+	} `json:"registry_counts"`
+	Hosts struct {
+		Total             int         `json:"total"`
+		DisconnectedCount int         `json:"disconnected_count"`
+		Hosts             interface{} `json:"hosts"`
+	} `json:"hosts"`
+	Alerts []struct {
+		ID           int    `json:"id"`
+		Time         int    `json:"time"`
+		Type         string `json:"type"`
+		User         string `json:"user"`
+		Image        string `json:"image"`
+		Imagehash    string `json:"imagehash"`
+		Container    string `json:"container"`
+		Containerid  string `json:"containerid"`
+		Host         string `json:"host"`
+		Hostid       string `json:"hostid"`
+		Category     string `json:"category"`
+		Result       int    `json:"result"`
+		UserResponse string `json:"user_response"`
+		Data         string `json:"data"`
+	} `json:"alerts"`
+	AuditTickers []struct {
+		ID          int    `json:"id"`
+		Time        int    `json:"time"`
+		Type        string `json:"type"`
+		User        string `json:"user"`
+		Action      string `json:"action"`
+		Image       string `json:"image"`
+		Imagehash   string `json:"imagehash"`
+		Container   string `json:"container"`
+		Containerid string `json:"containerid"`
+		Host        string `json:"host"`
+		Hostid      string `json:"hostid"`
+		Category    string `json:"category"`
+		Result      int    `json:"result"`
+		Data        string `json:"data"`
+	} `json:"audit_tickers"`
+}

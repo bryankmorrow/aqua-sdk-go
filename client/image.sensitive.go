e9f15e4dfa76ad1af7cce7c374e6953af7d098f4
Bryan Morrow <bkmorrow@gmail.com>
2020-03-12 09:44:12 -0500

    add single image sensitive call
    v2/images/registry/repo/tag/sensitive

--- client/image.sensitive.go
+++ client/image.sensitive.go
@@ -9,15 +9,15 @@ import (
 	"github.com/parnurzeal/gorequest"
 )
 
-// GetMalware -  retrieves malware for a single image based on registry, repo and tag
+// GetSensitive -  retrieves sensitive data for a single image based on registry, repo and tag
 // Accepts the registry, repo and tag
-// Returns malware struct
+// Returns sensitive struct
 // v2/images/registry/repo/tag/sensitive
-func (cli *Client) GetMalware(registry, repo, tag string) images.Malware {
-	var response = images.Malware{}
+func (cli *Client) GetSensitive(registry, repo, tag string) images.Sensitive {
+	var response = images.Sensitive{}
 	request := gorequest.New()
 	request.Set("Authorization", "Bearer "+cli.token)
-	apiPath := fmt.Sprintf("/api/v2/images/%s/%s/%s/malware", registry, repo, tag)
+	apiPath := fmt.Sprintf("/api/v2/images/%s/%s/%s/sensitive", registry, repo, tag)
 	events, body, errs := request.Clone().Get(cli.url + apiPath).End()
 	log.Printf("Calling %s%s", cli.url, apiPath)
 	if errs != nil {
@@ -26,7 +26,7 @@ func (cli *Client) GetMalware(registry, repo, tag string) images.Malware {
 	if events.StatusCode == 200 {
 		err := json.Unmarshal([]byte(body), &response)
 		if err != nil {
-			log.Printf("Error calling func GetMalware from %s%s, %v ", cli.url, apiPath, err.Error())
+			log.Printf("Error calling func GetSensitive from %s%s, %v ", cli.url, apiPath, err.Error())
 			//json: Unmarshal(non-pointer main.Request)
 		}
 	}
--- types/images/sensitive.go
+++ types/images/sensitive.go
@@ -1,1 +1,19 @@
 package images
+
+import "time"
+
+// Sensitive - response from v2/images/registry/repo/tag/sensitive
+type Sensitive struct {
+	Count    int `json:"count"`
+	Page     int `json:"page"`
+	Pagesize int `json:"pagesize"`
+	Result   []struct {
+		Type             string    `json:"type"`
+		Path             string    `json:"path"`
+		Hash             string    `json:"hash"`
+		Filename         string    `json:"filename"`
+		Acknowledged     bool      `json:"acknowledged"`
+		AcknowledgeDate  time.Time `json:"acknowledge_date"`
+		AcknowledgeScope string    `json:"acknowledge_scope"`
+	} `json:"result"`
+}

2db70813083411eae3790695df19b4f523d85133
Bryan Morrow <bkmorrow@gmail.com>
2020-03-13 10:09:37 -0500

    infra detail by name and id

---
+++ client/infra_detail.go
@@ -0,0 +1,56 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/BryanKMorrow/aqua-sdk-go/types/infrastructure"
+	"github.com/parnurzeal/gorequest"
+	"log"
+)
+
+// GetDetailName - gets a single infrastructure asset by name
+// Path v2/infrastructure/<type>/<name> (type can be cluster or node
+// Return - infrastructure.Detail response struct
+func (cli *Client) GetDetailName(infraType, name string) infrastructure.Detail {
+	var response = infrastructure.Detail{}
+	request := gorequest.New()
+	request.Set("Authorization", "Bearer "+cli.token)
+	apiPath := fmt.Sprintf("/api/v2/infrastructure/%s/%s", infraType, name)
+	events, body, errs := request.Clone().Get(cli.url + apiPath).End()
+	log.Printf("Calling %s%s", cli.url, apiPath)
+	if errs != nil {
+		log.Println(events.StatusCode)
+	}
+	if events.StatusCode == 200 {
+		err := json.Unmarshal([]byte(body), &response)
+		if err != nil {
+			log.Printf("Error calling func GetDetailName from %s%s, %v ", cli.url, apiPath, err.Error())
+			//json: Unmarshal(non-pointer main.Request)
+		}
+	}
+	return response
+}
+
+// GetDetailID - gets a single infrastructure asset by ID
+// Path v2/infrastructure/<id>
+// Return - infrastructure.Detail response struct
+// NOTE - may only work for Nodes, not Clusters??
+func (cli *Client) GetDetailID(ID string) infrastructure.Detail {
+	var response = infrastructure.Detail{}
+	request := gorequest.New()
+	request.Set("Authorization", "Bearer "+cli.token)
+	apiPath := fmt.Sprintf("/api/v2/infrastructure/%s", ID)
+	events, body, errs := request.Clone().Get(cli.url + apiPath).End()
+	log.Printf("Calling %s%s", cli.url, apiPath)
+	if errs != nil {
+		log.Println(events.StatusCode)
+	}
+	if events.StatusCode == 200 {
+		err := json.Unmarshal([]byte(body), &response)
+		if err != nil {
+			log.Printf("Error calling func GetDetailID from %s%s, %v ", cli.url, apiPath, err.Error())
+			//json: Unmarshal(non-pointer main.Request)
+		}
+	}
+	return response
+}
---
+++ types/infrastructure/detail.go
@@ -0,0 +1,93 @@
+package infrastructure
+
+import "time"
+
+// Detail is the infrastructure response for v2/infrastructure/type/name or /id
+type Detail struct {
+	ID                     int    `json:"id"`
+	ClusterID              int    `json:"cluster_id"`
+	NodeID                 string `json:"node_id"`
+	Name                   string `json:"name"`
+	Type                   string `json:"type"`
+	ClusterVulnerabilities struct {
+		Lastupdate     int `json:"lastupdate"`
+		Kubehunterscan struct {
+			Ok    int    `json:"ok"`
+			Low   int    `json:"low"`
+			High  int    `json:"high"`
+			Kburl string `json:"kburl"`
+			Nodes []struct {
+				Type     string `json:"type"`
+				Location string `json:"location"`
+			} `json:"nodes"`
+			Medium   int `json:"medium"`
+			Services []struct {
+				Type     string `json:"type"`
+				Service  string `json:"service"`
+				Location string `json:"location"`
+			} `json:"services"`
+			Vulnerabilities []struct {
+				URL           string `json:"url"`
+				Vid           string `json:"vid"`
+				Hunter        string `json:"hunter"`
+				Category      string `json:"category"`
+				Evidence      string `json:"evidence"`
+				Location      string `json:"location"`
+				Severity      string `json:"severity"`
+				Description   string `json:"description"`
+				Vulnerability string `json:"vulnerability"`
+			} `json:"vulnerabilities"`
+			HunterStatistics []struct {
+				Name            string `json:"name"`
+				Description     string `json:"description"`
+				Vulnerabilities int    `json:"vulnerabilities"`
+			} `json:"hunter_statistics"`
+		} `json:"kubehunterscan"`
+	} `json:"cluster_vulnerabilities,omitempty"`
+	Data struct {
+		Roles struct {
+			ClusterRoles []struct {
+				UID   string `json:"uid"`
+				Name  string `json:"name"`
+				Rules []struct {
+					Verbs     []string `json:"verbs"`
+					Apigroups []string `json:"apigroups"`
+					Resources []string `json:"resources"`
+				} `json:"rules"`
+				Labels          []string `json:"labels,omitempty"`
+				Creationtime    string   `json:"creationtime"`
+				Resourcecount   int      `json:"resourcecount,omitempty"`
+				Resourceversion string   `json:"resourceversion"`
+			} `json:"cluster_roles"`
+		} `json:"roles"`
+		Information struct {
+			ServerInfo struct {
+				Major      string    `json:"major"`
+				Minor      string    `json:"minor"`
+				Platform   string    `json:"platform"`
+				Builddate  time.Time `json:"builddate"`
+				Gitversion string    `json:"gitversion"`
+			} `json:"server_info"`
+			ComponentsStatusList []struct {
+				Name       string `json:"name"`
+				Conditions []struct {
+					Type    string `json:"type"`
+					Status  string `json:"status"`
+					Message string `json:"message"`
+				} `json:"conditions"`
+				Creationtime string `json:"creationtime"`
+			} `json:"components_status_list"`
+		} `json:"information"`
+	} `json:"data,omitempty"`
+	SecurityIssues struct {
+		CritVulns    int `json:"crit_vulns"`
+		HighVulns    int `json:"high_vulns"`
+		MedVulns     int `json:"med_vulns"`
+		LowVulns     int `json:"low_vulns"`
+		NegVulns     int `json:"neg_vulns"`
+		Malware      int `json:"malware"`
+		LastVulnScan int `json:"last_vuln_scan"`
+	} `json:"security_issues,omitempty"`
+	IsEnforced  bool      `json:"is_enforced"`
+	CreatedDate time.Time `json:"created_date"`
+}

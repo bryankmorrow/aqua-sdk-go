aed01dc4f97b1521381931e7c8ec1aee923d05eb
Bryan Morrow <bkmorrow@gmail.com>
2020-03-13 09:46:55 -0500

    add infrastructure query

---
+++ client/infra_list.go
@@ -0,0 +1,43 @@
+package client
+
+import (
+	"encoding/json"
+	"github.com/BryanKMorrow/aqua-sdk-go/types/infrastructure"
+	"github.com/parnurzeal/gorequest"
+	"log"
+	"strconv"
+)
+
+// GetInfrastructure - retrieves all infrastructure
+// Query Parameters are  page number, pagesize and string and bool  map
+// Returns response struct, remaining count and next page
+// v2/infrastructure
+func (cli *Client) GetInfrastructure(page, pagesize int, paramsString map[string]string, paramsBool map[string]bool) (infrastructure.List, int, int, int) {
+	// set the default pagesize
+	if pagesize == 0 {
+		pagesize = 1000
+	}
+	var response = infrastructure.List{}
+	request := gorequest.New()
+	request.Set("Authorization", "Bearer "+cli.token)
+	apiPath := "/api/v2/infrastructure"
+	paramString := cli.GetStringParams(paramsString)
+	paramBool := cli.GetBoolParams(paramsBool)
+	events, body, errs := request.Clone().Get(cli.url+apiPath).Param("page", strconv.Itoa(page)).Param("pagesize", strconv.Itoa(pagesize)).
+		Query(paramString).Query(paramBool).End()
+	log.Printf("Calling %s%s", cli.url, apiPath)
+	if errs != nil {
+		log.Println(events.StatusCode)
+	}
+	if events.StatusCode == 200 {
+		err := json.Unmarshal([]byte(body), &response)
+		if err != nil {
+			log.Printf("Error calling func GetInfrastructure from %s%s, %v ", cli.url, apiPath, err.Error())
+			//json: Unmarshal(non-pointer main.Request)
+		}
+	}
+	remaining := cli.CalcRemaining(pagesize, page, response.Count)
+	page = response.Page + 1
+	remaining, next := cli.CalcNext(remaining, page)
+	return response, remaining, next, response.Count
+}

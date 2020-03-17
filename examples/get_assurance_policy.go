51650fc46ab8cea529692bdaa7acbc6953f75996
Bryan Morrow <bkmorrow@gmail.com>
2020-03-16 20:45:09 -0500

    Create example for retrieving Assurance policies

---
+++ examples/get_assurance_policy.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/BryanKMorrow/aqua-sdk-go/client"
+	"log"
+	"os"
+)
+
+func main() {
+	// Get the Aqua CSP connection parameters from Environment Variables
+	url := os.Getenv("AQUA_URL")
+	user := os.Getenv("AQUA_USER")
+	password := os.Getenv("AQUA_PASSWORD")
+
+	// Create the client and get the JWT token for API call authorization
+	cli := client.NewClient(url, user, password)
+	connected := cli.GetAuthToken()
+	if !connected {
+		log.Fatalln("Failed to retrieve JWT Authorization Token")
+	} else {
+		// Get assurance policy identifiers only, not the detailed data.
+		params := make(map[string]string)
+		params["identifiers_only"] = "true"
+		detail := cli.GetAssurancePolicies(params)
+		for _, d := range detail.Result {
+			log.Printf("Type: %s   Name: %s  Description: %s  Last Updated: %v", d.AssuranceType, d.Name, d.Description, d.Lastupdate)
+		}
+	}
+}

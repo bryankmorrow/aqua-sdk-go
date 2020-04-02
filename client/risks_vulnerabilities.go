417c94447a9627de0e6df90e9f8ac50508c4e9ce
Bryan Morrow <bkmorrow@gmail.com>
2020-04-02 11:11:22 -0500

    get risk vulnerability + actionable vuln count example

---
+++ client/risks_vulnerabilities.go
@@ -0,0 +1,38 @@
+package client // import "github.com/BryanKMorrow/aqua-sdk-go/client"
+import (
+	"encoding/json"
+	"github.com/BryanKMorrow/aqua-sdk-go/types/risks"
+	"github.com/parnurzeal/gorequest"
+	"log"
+	"strconv"
+)
+
+// GetRiskVulnerabilities - retrieves all at risk vulnerabilities
+// Returns Repository struct
+// Path - api/v2/repositories
+func (cli *Client) GetRiskVulnerabilities(page, pagesize int, paramsString map[string]string) risks.Vulnerabilities {
+	// set the default pagesize
+	if pagesize == 0 {
+		pagesize = 1000
+	}
+	var response = risks.Vulnerabilities{}
+	request := gorequest.New()
+	request.Set("Authorization", "Bearer "+cli.token)
+	apiPath := "/api/v2/risks/vulnerabilities"
+	paramString := cli.GetStringParams(paramsString)
+	events, body, errs := request.Clone().Get(cli.url+apiPath).Param("page", strconv.Itoa(page)).Param("pagesize", strconv.Itoa(pagesize)).
+		Query(paramString).End()
+	log.Printf("Calling %s%s", cli.url, apiPath)
+	if errs != nil {
+		log.Println(events.StatusCode)
+	}
+	if events.StatusCode == 200 {
+		err := json.Unmarshal([]byte(body), &response)
+		if err != nil {
+			log.Printf("Error calling func GetRiskVulnerabilities from %s%s, %v ", cli.url, apiPath, err.Error())
+			//json: Unmarshal(non-pointer main.Request)
+		}
+	}
+
+	return response
+}
---
+++ examples/get_actionable_vuln_count.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/BryanKMorrow/aqua-sdk-go/client"
+	"log"
+	"os"
+)
+
+func main() {
+	// Get the Aqua CSP connection parameters from Environment Variables
+	url := os.Getenv("AQUA_URL")
+	user := os.Getenv("AQUA_USER")
+	password := os.Getenv("AQUA_PASSWORD")
+
+	// Create the client and get the JWT token for API call authorization
+	cli := client.NewClient(url, user, password)
+	connected := cli.GetAuthToken()
+	if !connected {
+		log.Fatalln("Failed to retrieve JWT Authorization Token")
+	} else {
+		params := make(map[string]string)
+		params["include_vpatch_info"] = "true"
+		params["show_negligible"] = "true"
+		params["skip_count"] = "false"
+		params["hide_base_image"] = "false"
+		params["fix_availability"] = "true"
+		params["order_by"] = "-image"
+		detail := cli.GetRiskVulnerabilities(1, 50, params)
+		log.Println(detail.Count)
+	}
+}
